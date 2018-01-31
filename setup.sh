f80aca47654b44e426b82b683fdd515105f8eeae
Arian Bessonart <arianbessonart@gmail.com>
2018-01-31 11:26:08 -0300

    add git aliases

--- setup.sh
+++ setup.sh
@@ -31,6 +31,12 @@ else
   e_error "Not set"
 fi
 
+## Git alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
 ## Install oh-my-zsh
 
 ZSH=~/.oh-my-zsh
