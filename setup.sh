39c9384fa08b86ce9188c2257fbc0a879c5350f2
Arian Bessonart <arianbessonart@gmail.com>
2018-01-30 21:47:01 -0300

    add git config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,32 @@
 source utility.sh
 
 
+
+
+# Git configuration
+
+e_header "Setup git config (global)"
+cp gitignore ~/.gitignore_global  ## Adding .gitignore global
+git config --global core.excludesfile "${HOME}/.gitignore_global"
+
+ask "${blue} (Option) Enter Your Git Email: "
+read -r emailId
+if is_empty $emailId; then
+  git config --global user.email "$emailId" ## Git Email Id
+  e_success "Email is set"
+else
+  e_error "Not set"
+fi
+
+ask "${blue} (Option) Enter Your Git Username: "
+read -r userName
+if is_empty $userName; then
+  git config --global user.name "$userName" ## Git Username
+  e_success "Username is set"
+else
+  e_error "Not set"
+fi
+
 ## Install oh-my-zsh
 
 ZSH=~/.oh-my-zsh
@@ -29,3 +55,5 @@ else
   ## Autosuggest
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
+
+echo "🍺🍺🍺🍺  Thats all, Done."
