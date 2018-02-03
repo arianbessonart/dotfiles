43f06a62dfd4213db5900fc0e07a438df8209f0e
Arian Bessonart <arianbessonart@gmail.com>
2018-02-03 18:15:50 -0300

    add apps to install

--- brew.sh
+++ brew.sh
@@ -1,13 +1,30 @@
-# Install Homebrew
+#!/usr/bin/env bash
 
+# Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew upgrade
 
 apps=(
+    coreutils
+    tree
     git
     vim
     wget
 )
 
 brew install "${apps[@]}"
+
+brew tap caskroom/cask
+
+appsCask=(
+    google-chrome
+    iterm2
+    lastpass
+    spotify
+    sublime-text
+    visual-studio-code
+    the-unarchiver
+)
+
+brew cask install "${appsCask[@]}"
