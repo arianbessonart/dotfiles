b4a1e8e3dce19a52dd4b055a2ebe8026b9881683
Arian Bessonart <arianbessonart@gmail.com>
2026-01-14 15:11:41 -0300

    more fixes

--- homebrew/install-apps.sh
+++ homebrew/install-apps.sh
@@ -32,11 +32,10 @@ done
 
 appsCask=(
   "ghostty"
-  "brave"
+  "brave-browser"
   "cursor"
   "chatgpt"
-  "docker-desktop"
-  "claude-code"
+  "docker"
   "google-cloud-sdk"
   "itsycal"
   "meetingbar"
@@ -44,7 +43,6 @@ appsCask=(
   "notion"
   "notion-calendar"
   "postman"
-  "stremio"
   "slack"
   "rectangle"
   "spotify"
@@ -54,10 +52,10 @@ appsCask=(
 
 for appCask in "${appsCask[@]}"
 do
-  if ! brew info --cask "$appCask" > /dev/null; then
-    fancy_echo "$appCask installed"
+  if brew list --cask "$appCask" &>/dev/null; then
+    fancy_echo "$appCask already installed"
   else
-    echo "$appCask is not installed"
+    echo "Installing $appCask..."
     brew install --cask "$appCask"
   fi
 done
